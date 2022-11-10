e38bbba899850142e5bc19e42b7d8b1630605b2c
kazenone <kazenone@gmail.com>
2022-11-10 00:24:35 +0000

    Basic retrieval of server details for /manage page

--- server.js
+++ server.js
@@ -137,6 +137,43 @@ app.get('/login', (request, response) => {
     .catch(console.error)
 });
 
+app.get('/manage/:serverId', (request, response) => {
+  if (!request.cookies['oauth']) {
+      response.redirect('/');
+    return;
+  }
+ 
+  var serverId = request.params.serverId;
+ 
+  // Check we are allowed to manage this server 
+  getHeaderInfo(request)
+    .then(header => {
+      return fetch(`https://discord.com/api/users/@me/guilds?after=${serverId-1}&limit=1`, getAuth(request))
+          .then(result => result.json())
+          .then(guilds => {
+            if (!Array.isArray(guilds)) {
+              throw guilds;
+            }
+            
+            var guild = guilds[0];
+            if (guild.id !== serverId || guild.permissions & 32 == 0) { // Don't have manage permissions on this server
+              return response.status(403).send('403 Forbidden'); // Custom error pages later?
+            }
+            
+            db.collection('meta').doc(serverId).get()
+              .then(doc => {
+                if (!doc.exists) {
+                  return response.status(404).send('404 Not Found');
+                }
+                
+                console.log(doc.data());
+                response.render('manage', { server: doc.data(), header: header });
+              });
+      });
+    })
+    .catch(console.error);
+});
+
 async function getHeaderInfo(request) {
   if (!request.cookies['oauth']) {
     return {
